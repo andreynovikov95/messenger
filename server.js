2262d16b815e80f6f2df633e236ec0b00afbd803
Andrey Novikov <NovikovAA.it@relef.ru>
2020-06-14 22:00:00 +0300

    YTASK-3: change directory

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('./dist'));
+app.use(express.static(path.join(__dirname, '/static')));
 
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`);
